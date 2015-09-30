3a03acfbfba9dc615b63f045b5be979fa9a7f2b7
Axel Smeets <murlocbrand+git@gmail.com>
2015-10-01 00:07:17 +0200

    npmify pcurl

--- pcurl.js
+++ pcurl.js
@@ -2,19 +2,21 @@
 var lz = require('lz-string'),
     req = require('request')
 
-if (process.argv.length < 2) {
-    console.error("usage: node pcurl <station-url>")
-    process.exit(1)
+module.export = function pcurl (station, callback) {
+    var url = 'https://piratrad.io/' + station + '/tracks'
+    req(url, function (err, res, body) {
+        if (err) {
+            callback(err, [])
+            return
+        }
+
+        var list = JSON.parse(body).unresolvedList
+        for (var i = 0; i < list.length; i++) {
+            var url = JSON.parse(list[i]).url
+            list[i] = lz.decompressFromUTF16(url)
+        }
+
+        callback(undefined, list)
+    })
+
 }
-
-var station = process.argv[2]
-
-req('https://piratrad.io/' + station + '/tracks', function (err, res, body) {
-    var list = JSON.parse(body).unresolvedList
-    for (var i = 0; i < list.length; i++) {
-        var url = JSON.parse(list[i]).url
-
-        process.stdout.write(lz.decompressFromUTF16(url))
-        process.stdout.write('\n')
-    }
-})
